dd6f2d697a41c24711fb86449e1d0a957fb86cb3
Dennis <488132230@qq.com>
2024-10-20 14:12:19 +0800

    初始化0_2版本，需要判断是否是角度传感器获取角度太慢了导致速度上来以后旋转变得卡顿，待验证

---
+++ Tsfoc_Soft_V0_1/tsfoc_V0.2/app/sensor/as5600_hardware.h
@@ -0,0 +1,29 @@
+/*
+ * as5600_hardware.h    
+ *
+ *  Created on: 2024-10-20
+ *      Author: Dennis
+ *      E-mail: <dennisreyoonjiho@gmail.com>
+ *      Description: This file contains the hardware interface for the AS5600 sensor.
+ *      The interface is based on the I2C protocol.
+ *      The sensor is used to measure the angle of the shaft of a motor.
+*/
+
+#ifndef AS5600_HARDWARE_H
+#define AS5600_HARDWARE_H
+
+#include "i2c.h"
+#include "user_api.h"
+
+
+
+
+
+
+
+
+
+#endif // AS5600_HARDWARE_H
+
+
+
